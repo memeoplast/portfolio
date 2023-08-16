14c12765226455ca0a52ab2fa2610dfcc5e974e2
Nico G <91379265+memeoplast@users.noreply.github.com>
2023-08-16 11:14:52 +0200

    Intial clean slate set-up with TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='block__content' >
+      <h1 className='heading__primary'>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Routes/error-page.d.ts
@@ -0,0 +1,9 @@
+declare module 'error-page' {
+  import React from 'react';
+
+  interface ErrorPageProps {}
+
+  const ErrorPage: React.FC<ErrorPageProps>;
+
+  export default ErrorPage;
+}
