3661f8c2056f63d59f5cc3e6b4dbd785e07d35ad
Nico G <91379265+memeoplast@users.noreply.github.com>
2023-08-16 13:46:04 +0200

    basic header config

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="block__content">
+      <div className="header__background">
+        <h1 className="heading__primary">Header</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
