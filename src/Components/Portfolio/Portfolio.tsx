09eb5d5db4f9163ab3ef6807dde1fa6408e2b2c2
Nico G <91379265+memeoplast@users.noreply.github.com>
2023-08-18 09:15:10 +0200

    Grid style and first hover effects are done.

--- src/Components/Portfolio/Portfolio.tsx
+++ src/Components/Portfolio/Portfolio.tsx
@@ -3,19 +3,73 @@ import "./Portfolio.css";
 
 const Portfolio = () => {
   return (
-    <div className="block__content">
+    <div className="Portfolio__block-content">
       <div className="Portfolio__grid">
         <div className="Portfolio__card">
-          <p className="Portfolio__card-heading">Project 1</p>
+          <div className="Portfolio__card-heading-container">
+            <p className="Portfolio__card-heading">Project 1</p>
+            <div className="Portfolio__card-heading-underline"></div>
+          </div>
+          <a
+            className="Portfolio__link"
+            href="https://memeoplast.github.io/odin-page1/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="Portfolio__img"
+              src="src/assets/Portfolio.jpg"
+            ></img>
+          </a>
         </div>
         <div className="Portfolio__card">
-          <p className="Portfolio__card-heading">Project 2</p>
+          <div className="Portfolio__card-heading-container">
+            <p className="Portfolio__card-heading">Project 2</p>
+            <div className="Portfolio__card-heading-underline"></div>
+          </div>
+          <a
+            className="Portfolio__link"
+            href="https://memeoplast.github.io/react-app/"
+            target="_blank"
+          >
+            <img
+              className="Portfolio__img"
+              src="src/assets/Portfolio.jpg"
+            ></img>
+          </a>
         </div>
         <div className="Portfolio__card">
-          <p className="Portfolio__card-heading">Project 3</p>
+          <div className="Portfolio__card-heading-container">
+            <p className="Portfolio__card-heading">Project 3</p>
+            <div className="Portfolio__card-heading-underline"></div>
+          </div>
+
+          <a
+            className="Portfolio__link"
+            href="https://memeoplast.github.io/react-app/"
+            target="_blank"
+          >
+            <img
+              className="Portfolio__img"
+              src="src/assets/Portfolio.jpg"
+            ></img>
+          </a>
         </div>
         <div className="Portfolio__card">
-          <p className="Portfolio__card-heading">Project 4</p>
+          <div className="Portfolio__card-heading-container">
+            <p className="Portfolio__card-heading">Project 4</p>
+            <div className="Portfolio__card-heading-underline"></div>
+          </div>
+          <a
+            className="Portfolio__link"
+            href="https://memeoplast.github.io/react-app/"
+            target="_blank"
+          >
+            <img
+              className="Portfolio__img"
+              src="src/assets/Portfolio.jpg"
+            ></img>
+          </a>
         </div>
       </div>
     </div>
