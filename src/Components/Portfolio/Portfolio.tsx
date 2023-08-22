8281bb1324e3c0c4b86c877a2a400fe61ad3eefa
Nico G <91379265+memeoplast@users.noreply.github.com>
2023-08-22 10:21:30 +0200

    corrected spelling

--- src/Components/Portfolio/Portfolio.tsx
+++ src/Components/Portfolio/Portfolio.tsx
@@ -3,70 +3,70 @@ import "./Portfolio.css";
 
 const Portfolio = () => {
   return (
-    <div className="Portfolio__block-content">
-      <div className="Portfolio__grid">
-        <div className="Portfolio__card">
-          <div className="Portfolio__card-heading-container">
-            <p className="Portfolio__card-heading">Project 1</p>
-            <div className="Portfolio__card-heading-underline"></div>
+    <div className="portfolio__block-content">
+      <div className="portfolio__grid">
+        <div className="portfolio__card">
+          <div className="portfolio__card-heading-container">
+            <p className="portfolio__card-heading">Project 1</p>
+            <div className="portfolio__card-heading-underline"></div>
           </div>
           <a
-            className="Portfolio__link"
+            className="portfolio__link"
             href="https://memeoplast.github.io/odin-page1/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
-              className="Portfolio__img"
+              className="portfolio__img"
               src="src/assets/Portfolio.jpg"
             ></img>
           </a>
         </div>
-        <div className="Portfolio__card">
-          <div className="Portfolio__card-heading-container">
-            <p className="Portfolio__card-heading">Project 2</p>
-            <div className="Portfolio__card-heading-underline"></div>
+        <div className="portfolio__card">
+          <div className="portfolio__card-heading-container">
+            <p className="portfolio__card-heading">Project 2</p>
+            <div className="portfolio__card-heading-underline"></div>
           </div>
           <a
-            className="Portfolio__link"
+            className="portfolio__link"
             href="https://memeoplast.github.io/react-app/"
             target="_blank"
           >
             <img
-              className="Portfolio__img"
+              className="portfolio__img"
               src="src/assets/Portfolio.jpg"
             ></img>
           </a>
         </div>
-        <div className="Portfolio__card">
-          <div className="Portfolio__card-heading-container">
-            <p className="Portfolio__card-heading">Project 3</p>
-            <div className="Portfolio__card-heading-underline"></div>
+        <div className="portfolio__card">
+          <div className="portfolio__card-heading-container">
+            <p className="portfolio__card-heading">Project 3</p>
+            <div className="portfolio__card-heading-underline"></div>
           </div>
 
           <a
-            className="Portfolio__link"
+            className="portfolio__link"
             href="https://memeoplast.github.io/react-app/"
             target="_blank"
           >
             <img
-              className="Portfolio__img"
+              className="portfolio__img"
               src="src/assets/Portfolio.jpg"
             ></img>
           </a>
         </div>
-        <div className="Portfolio__card">
-          <div className="Portfolio__card-heading-container">
-            <p className="Portfolio__card-heading">Project 4</p>
-            <div className="Portfolio__card-heading-underline"></div>
+        <div className="portfolio__card">
+          <div className="portfolio__card-heading-container">
+            <p className="portfolio__card-heading">Project 4</p>
+            <div className="portfolio__card-heading-underline"></div>
           </div>
           <a
-            className="Portfolio__link"
+            className="portfolio__link"
             href="https://memeoplast.github.io/react-app/"
             target="_blank"
           >
             <img
-              className="Portfolio__img"
+              className="portfolio__img"
               src="src/assets/Portfolio.jpg"
             ></img>
           </a>
